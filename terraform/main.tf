b3c065e3eec90dd421a2f2052d348a57801f6806
Kohli <karkohli@38f9d35d7e0b.ant.amazon.com>
2022-05-02 14:34:44 -0400

    adding existing account ctactive

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,32 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+
+module "ctactive" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "karkohli+6@amazon.com"
+    AccountName               = "ctactive"
+    ManagedOrganizationalUnit = "Enroll_test"
+    SSOUserEmail              = "karkohli@amazon.com"
+    SSOUserFirstName          = "AWS Control Tower"
+    SSOUserLastName           = "Admin"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
